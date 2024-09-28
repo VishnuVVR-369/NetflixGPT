880d1419d5ea414e36672913e20b2009ea9b50bf
VishnuVVR-369 <vishnuvardhanganji@gmail.com>
2024-09-28 15:58:41 +0530

    Scaffolding and planning

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="flex font-bold underline">App Initalized!!!</div>
+        </>
+    );
+}
+
+export default App;
